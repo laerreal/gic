82243bd3ffa52ea9143d6ecc3af5e1b8e084dcf6
Efimov Vasily <real@ispras.ru>
2017-09-08 14:12:19 +0300

    GIC: add CLI arguments "--source" and "--destination" (-d)
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- gic.py
+++ gic.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 
+from argparse import \
+    ArgumentTypeError, \
+    ArgumentParser
+from os.path import isdir
+
+def arg_type_directory(string):
+    if not isdir(string):
+        raise ArgumentTypeError(
+            "{} is not directory".format(string))
+    return string
+
 def main():
     print("Git Interactive Cloner")
+
+    ap = ArgumentParser()
+    ap.add_argument("source", type = arg_type_directory, nargs = 1)
+    ap.add_argument("-d", "--destination", type = arg_type_directory, nargs = 1)
+
+    args = ap.parse_args()
 
 if __name__ == "__main__":
     ret = main()

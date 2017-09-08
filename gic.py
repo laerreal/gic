5a78f4e47e4096c68b795f21469d4f1aa2a0ec6b
Efimov Vasily <real@ispras.ru>
2017-09-08 14:10:35 +0300

    GIC: add `main` function
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- gic.py
+++ gic.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+def main():
     print("Git Interactive Cloner")
+
+if __name__ == "__main__":
+    ret = main()
+    exit(0 if ret is None else ret)

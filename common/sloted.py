d1ec1834c81557778fba9a63b5eb6ca79b2456f1
Efimov Vasily <real@ispras.ru>
2017-09-21 17:57:34 +0300

    common.sloted: python serialization support
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/sloted.py
+++ common/sloted.py
@@ -1,6 +1,8 @@
 __all__ = ["sloted"]
 
 class sloted(object):
+    __slots__ = ["__dfs_visited__"]
+
     def __init__(self, **kw):
         for klass in type(self).__mro__:
             try:
@@ -12,3 +14,33 @@ class sloted(object):
                 if attr.startswith("_"):
                     continue
                 setattr(self, attr, kw[attr])
+
+    def gen_by_slots(self, gen, **extra):
+        """ Helper for implementation of __gen_code__ for PyGenerator API. """
+        all_slots = []
+        for klass in type(self).__mro__:
+            try:
+                slots = klass.__slots__
+            except AttributeError:
+                continue
+
+            for attr in reversed(slots):
+                if attr in all_slots:
+                    continue
+                all_slots.append(attr)
+
+        gen.reset_gen(self)
+
+        for attr in reversed(all_slots):
+            if attr.startswith("_"):
+                continue
+            gen.gen_field(attr + " = ")
+            try:
+                gen.pprint(getattr(self, attr))
+            except BaseException as e:
+                raise e
+
+        for k, v in extra.items():
+            gen.gen_field(k + " = " + gen.gen_const(v))
+
+        gen.gen_end()

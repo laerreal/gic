38dc70b5aa5f1c5985b227d340b6de9282524b41
Efimov Vasily <real@ispras.ru>
2017-11-21 14:20:30 +0300

    sloted: `gen_by_slots` skips keyword argument default values
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- common/sloted.py
+++ common/sloted.py
@@ -1,5 +1,7 @@
 __all__ = ["sloted"]
 
+from .reflection import get_class_total_args
+
 class sloted(object):
     __slots__ = ["__dfs_visited__"]
 
@@ -17,8 +19,10 @@ class sloted(object):
 
     def gen_by_slots(self, gen, **extra):
         """ Helper for implementation of __gen_code__ for PyGenerator API. """
+        self_type = type(self)
+
         slots2gen = []
-        for klass in type(self).__mro__:
+        for klass in self_type.__mro__:
             try:
                 slots = klass.__slots__
             except AttributeError:
@@ -31,11 +35,18 @@ class sloted(object):
                     continue
                 slots2gen.append(attr)
 
+        _, defaults = get_class_total_args(self_type)
+
         gen.reset_gen(self)
 
         for attr in reversed(slots2gen):
+            val = getattr(self, attr)
+
+            if (attr in defaults) and (defaults[attr] == val):
+                continue
+
             gen.gen_field(attr + " = ")
-            gen.pprint(getattr(self, attr))
+            gen.pprint(val)
 
         for k, v in extra.items():
             gen.gen_field(k + " = " + gen.gen_const(v))
